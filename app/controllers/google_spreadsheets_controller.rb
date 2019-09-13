a9c14cfcb44dcd381ba0d7f27caab888be50022d
mariya <mashulikmashulik@yahoo.com>
2019-09-13 17:24:10 +0300

    Add indexes

--- app/controllers/google_spreadsheets_controller.rb
+++ app/controllers/google_spreadsheets_controller.rb
@@ -3,6 +3,7 @@
 class GoogleSpreadsheetsController < ApplicationController
   before_action :google_auth
 
+
   def index
     @state = params[:state]
     @speakers = @speakers_worksheet.rows
@@ -14,16 +15,16 @@ class GoogleSpreadsheetsController < ApplicationController
 
   def create
     if @bought_tickets_worksheet.rows.flatten.include?(google_params['email'])
-      if @registered_users_worksheet.rows.map { |row| row[2] }.exclude?(google_params['email'])
+      # if @registered_users_worksheet.rows.map { |row| row[2] }.exclude?(google_params['email'])
         row = [google_params[:speaker], google_params[:timeslot], google_params[:email], google_params[:name],
                google_params[:phone], google_params[:note]]
         @registered_users_worksheet.insert_rows(@registered_users_worksheet.num_rows + 1, [row])
         @registered_users_worksheet.save
 
         redirect_to action: 'index', state: 'successfully_registered'
-      else
-        redirect_to action: 'index', state: 'already_registered'
-      end
+      # else
+      #   redirect_to action: 'index', state: 'already_registered'
+      # end
     else
       redirect_to action: 'index', state: 'need_registration'
     end
