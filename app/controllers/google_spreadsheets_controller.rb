4ddd72b31a2a08e7900404ffaa12f3e9e4a93b96
mariya <mashulikmashulik@yahoo.com>
2019-09-16 19:19:16 +0300

    Made changes in controller

--- app/controllers/google_spreadsheets_controller.rb
+++ app/controllers/google_spreadsheets_controller.rb
@@ -14,21 +14,17 @@ class GoogleSpreadsheetsController < ApplicationController
   end
 
   def create
-    if @bought_tickets_worksheet.rows.flatten.include?(params[:email]) && @emails.count { |email| email == params[:email] } < 3
-      # if  @emails.count { |email| email == params[:email] } > 3
-      # if @registered_users_worksheet.rows.map { |row| row[2] }.exclude?(google_params['email'])
-        row = [google_params[:speaker], google_params[:timeslot], google_params[:email], google_params[:name],
-               google_params[:phone], google_params[:note], google_params[:date]]
-        @registered_users_worksheet.insert_rows(@registered_users_worksheet.num_rows + 1, [row])
-        @registered_users_worksheet.save
+    if registration_allowed?
+      row = [google_params[:speaker], google_params[:timeslot], google_params[:email], google_params[:name],
+             google_params[:phone], google_params[:note], google_params[:date]]
+      @registered_users_worksheet.insert_rows(@registered_users_worksheet.num_rows + 1, [row])
+      @registered_users_worksheet.save
 
-        redirect_to action: 'index', state: 'successfully_registered'
-      # else
-      #   redirect_to action: 'index', state: 'already_registered'
-      # end
-
+      redirect_to action: 'index', state: 'successfully_registered'
+    elsif @registered_users_worksheet.rows.map { |row| row[2] }.exclude?(google_params['email'])
+          redirect_to action: 'index', state: 'need_registration'
     else
-      redirect_to action: 'index', state: 'need_registration'
+      redirect_to action: 'index', state: 'already_registered'
     end
   end
 
@@ -43,6 +39,11 @@ class GoogleSpreadsheetsController < ApplicationController
   end
 
   private
+
+  def registration_allowed?
+    @bought_tickets_worksheet.rows.flatten.include?(params[:email]) &&
+       @emails.count { |email| email == params[:email] } < 3
+  end
 
   def google_params
     params.permit(:speaker, :timeslot, :email, :name, :phone, :note, :date)
