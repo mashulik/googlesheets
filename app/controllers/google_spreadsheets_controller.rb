b8c433d9f5afad812ebac306d9e8fc882cd43d9a
mariya <mashulikmashulik@yahoo.com>
2019-09-16 15:22:53 +0300

    Add ability to registrate 3 users

--- app/controllers/google_spreadsheets_controller.rb
+++ app/controllers/google_spreadsheets_controller.rb
@@ -14,10 +14,11 @@ class GoogleSpreadsheetsController < ApplicationController
   end
 
   def create
-    if @bought_tickets_worksheet.rows.flatten.include?(google_params['email'])
+    if @bought_tickets_worksheet.rows.flatten.include?(params[:email]) && @emails.count { |email| email == params[:email] } < 3
+      # if  @emails.count { |email| email == params[:email] } > 3
       # if @registered_users_worksheet.rows.map { |row| row[2] }.exclude?(google_params['email'])
         row = [google_params[:speaker], google_params[:timeslot], google_params[:email], google_params[:name],
-               google_params[:phone], google_params[:note]]
+               google_params[:phone], google_params[:note], google_params[:date]]
         @registered_users_worksheet.insert_rows(@registered_users_worksheet.num_rows + 1, [row])
         @registered_users_worksheet.save
 
@@ -25,6 +26,7 @@ class GoogleSpreadsheetsController < ApplicationController
       # else
       #   redirect_to action: 'index', state: 'already_registered'
       # end
+
     else
       redirect_to action: 'index', state: 'need_registration'
     end
@@ -34,8 +36,8 @@ class GoogleSpreadsheetsController < ApplicationController
     session = GoogleDrive::Session.from_service_account_key('client_secret.json')
 
     spreadsheet = session.spreadsheet_by_title('DKINC')
-
     @registered_users_worksheet = spreadsheet.worksheet_by_title('Registered users')
+    @emails = @registered_users_worksheet.rows[1..-1].map { |row| row[2] }
     @bought_tickets_worksheet = spreadsheet.worksheet_by_title('Bought tickets')
     @speakers_worksheet = spreadsheet.worksheet_by_title('Speakers')
   end
@@ -43,6 +45,6 @@ class GoogleSpreadsheetsController < ApplicationController
   private
 
   def google_params
-    params.permit(:speaker, :timeslot, :email, :name, :phone, :note)
+    params.permit(:speaker, :timeslot, :email, :name, :phone, :note, :date)
   end
 end
